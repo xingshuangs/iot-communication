26f7b9c14ba578727e1181a9f302fb5ae6d62027
Shuang <xingshuang_cool@163.com>
2021-02-17 20:36:29 +0800

    chore: 初始化工程;

---
+++ src/main/java/com/github/oscura/App.java
@@ -0,0 +1,14 @@
+package com.github.oscura;
+
+/**
+ * Hello world!
+ *
+ * @author xingshuang
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
