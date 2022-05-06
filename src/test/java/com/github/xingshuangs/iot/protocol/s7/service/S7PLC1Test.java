63d00d35d683e66d2fbd7846055f58f6a1f85c5a
Shuang <xingshuang_cool@163.com>
2022-05-06 17:51:26 +0800

    feat: 修改S7PLC命名;

--- src/test/java/com/github/xingshuangs/iot/protocol/s7/service/S7PLC1Test.java
+++ src/test/java/com/github/xingshuangs/iot/protocol/s7/service/S7PLC1Test.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 
-public class S7PLCTest {
-    private S7PLC s7PLC = new S7PLC();
+public class S7PLC1Test {
+    private S7PLC1 s7PLC1 = new S7PLC1();
 
     @Test
     public void readUInt16() throws IOException {
-        int i = s7PLC.readUInt16("");
+        int i = s7PLC1.readUInt16("");
         assertEquals(1280, i);
     }
 }
