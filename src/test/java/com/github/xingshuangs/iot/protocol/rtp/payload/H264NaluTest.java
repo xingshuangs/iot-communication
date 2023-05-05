ed9dca754bdbeba8280249a498d34b84736fb1dd
Shuang <xingshuang_cool@163.com>
2023-05-05 21:31:08 +0800

    test: test H264NaluFuA and H264NaluSingle (测试H264NaluFuA和H264NaluSingle);

---
+++ src/test/java/com/github/xingshuangs/iot/protocol/rtp/payload/H264NaluTest.java
@@ -0,0 +1,69 @@
+package com.github.xingshuangs.iot.protocol.rtp.payload;
+
+import com.github.xingshuangs.iot.protocol.rtp.enums.EH264NaluType;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class H264NaluTest {
+
+    @Test
+    public void h264NaluFuA_Start() {
+        byte[] expect = new byte[]{(byte) 0x7C, (byte) 0x85, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        byte[] payload = new byte[]{(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        H264NaluFuA nalu = H264NaluFuA.fromBytes(expect);
+        assertFalse(nalu.header.isForbiddenZeroBit());
+        assertEquals(3, nalu.header.getNri());
+        assertEquals(EH264NaluType.FU_A, nalu.header.getType());
+        assertArrayEquals(payload, nalu.payload);
+
+        assertTrue(nalu.getFuHeader().isStart());
+        assertFalse(nalu.getFuHeader().isEnd());
+        assertFalse(nalu.getFuHeader().isReserve());
+        assertEquals(EH264NaluType.IDR_SLICE, nalu.getFuHeader().getType());
+    }
+
+    @Test
+    public void h264NaluFuA_Middle() {
+        byte[] expect = new byte[]{(byte) 0x7C, (byte) 0x05, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        byte[] payload = new byte[]{(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        H264NaluFuA nalu = H264NaluFuA.fromBytes(expect);
+        assertFalse(nalu.header.isForbiddenZeroBit());
+        assertEquals(3, nalu.header.getNri());
+        assertEquals(EH264NaluType.FU_A, nalu.header.getType());
+        assertArrayEquals(payload, nalu.payload);
+
+        assertFalse(nalu.getFuHeader().isStart());
+        assertFalse(nalu.getFuHeader().isEnd());
+        assertFalse(nalu.getFuHeader().isReserve());
+        assertEquals(EH264NaluType.IDR_SLICE, nalu.getFuHeader().getType());
+    }
+
+    @Test
+    public void h264NaluFuA_End() {
+        byte[] expect = new byte[]{(byte) 0x7C, (byte) 0x45, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        byte[] payload = new byte[]{(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        H264NaluFuA nalu = H264NaluFuA.fromBytes(expect);
+        assertFalse(nalu.header.isForbiddenZeroBit());
+        assertEquals(3, nalu.header.getNri());
+        assertEquals(EH264NaluType.FU_A, nalu.header.getType());
+        assertArrayEquals(payload, nalu.payload);
+
+        assertFalse(nalu.getFuHeader().isStart());
+        assertTrue(nalu.getFuHeader().isEnd());
+        assertFalse(nalu.getFuHeader().isReserve());
+        assertEquals(EH264NaluType.IDR_SLICE, nalu.getFuHeader().getType());
+    }
+
+    @Test
+    public void h264NaluSingle() {
+        byte[] expect = new byte[]{(byte) 0x61, (byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        byte[] payload = new byte[]{(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03};
+        H264NaluSingle nalu = H264NaluSingle.fromBytes(expect);
+        assertFalse(nalu.header.isForbiddenZeroBit());
+        assertEquals(3, nalu.header.getNri());
+        assertEquals(EH264NaluType.NON_IDR_SLICE, nalu.header.getType());
+        assertArrayEquals(payload, nalu.payload);
+    }
+}
