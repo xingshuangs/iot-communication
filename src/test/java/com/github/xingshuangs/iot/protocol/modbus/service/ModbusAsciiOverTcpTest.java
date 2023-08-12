d066d6099e05280f080c72f6a6abc12c7c045080
Shuang <xingshuang_cool@163.com>
2023-08-12 17:27:44 +0800

    test(modbus): forget ignore annotation (忘了@Ignore注解);

--- src/test/java/com/github/xingshuangs/iot/protocol/modbus/service/ModbusAsciiOverTcpTest.java
+++ src/test/java/com/github/xingshuangs/iot/protocol/modbus/service/ModbusAsciiOverTcpTest.java
@@ -3,14 +3,16 @@ package com.github.xingshuangs.iot.protocol.modbus.service;
 import com.github.xingshuangs.iot.utils.HexUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+@Ignore
 @Slf4j
 public class ModbusAsciiOverTcpTest {
 
-    private final ModbusAsciiOverTcp plc = new ModbusAsciiOverTcp(1, "127.0.0.1");
+    private final ModbusAsciiOverTcp plc = new ModbusAsciiOverTcp();
 
     @Before
     public void before() {
